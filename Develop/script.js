f103bd6453f55f39249d97b677460459d3322c53
lundyc0917 <lundyc0917@gmail.com>
2020-11-21 11:59:46 -0500

    added comments and TODOs

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,15 @@
+// Set Current day in Jumbotron
 var currentDayEl = $('#currentDay')
   
 function currentDay() {
   var rightNow = moment().format('dddd, MMMM DD, YYYY');
   currentDayEl.text(rightNow);
 }
-
+// Run currentDay function
 currentDay();
 
 
+// create For loop to change the background color of the time blocks
 for(i = 8; i < 18; i++) {
   var textEntry = "#hour" + i;
   var currentHour = moment().hour();
@@ -19,3 +21,5 @@ for(i = 8; i < 18; i++) {
     $(textEntry).addClass("present");
   }
 }
+
+// TODO Add save button action to save information in the text box to local storage
