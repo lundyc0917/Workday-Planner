6595994d1ca4c2c8c5898ac29fea895082202979
lundyc0917 <lundyc0917@gmail.com>
2020-11-21 11:53:33 -0500

    removed array not using. unneccessary

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,3 @@
-var hourArray = [
-  "8:00 AM",
-  "9:00 AM",
-  "10:00 AM",
-  "11:00 AM",
-  "12:00 PM",
-  "1:00 PM",
-  "2:00 PM",
-  "3:00 PM",
-  "4:00 PM",
-  "5:00 PM"
-];
-
 var currentDayEl = $('#currentDay')
   
 function currentDay() {
