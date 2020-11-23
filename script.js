59317a3edb7e35b0b8baaac76f268e1adc9b8d5b
lundyc0917 <lundyc0917@gmail.com>
2020-11-23 10:56:52 -0500

    local storage save function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var currentDayEl = $('#currentDay');
+var dayEntryEl = $('.description');
+var saveButtonEl = $('.saveBtn');
 
 
 // Set current date to Jumbotron
@@ -12,18 +14,23 @@ currentDay();
 
 // create For loop to change the background color of the time blocks
 for(i = 8; i < 18; i++) {
-  var textEntry = "#hour" + i;
+  var timeSlot = "#hour" + i;
   var currentHour = moment().hour();
 
   if(i < currentHour) {
-    $(textEntry).addClass("past");
+    $(timeSlot).addClass("past");
   } else if (i > currentHour) {
-    $(textEntry).addClass("future");
+    $(timeSlot).addClass("future");
   } else {
-    $(textEntry).addClass("present");
+    $(timeSlot).addClass("present");
   }
 }
 
 // TODO Add save button action to save information in the text box to local storage
 
+saveButtonEl.on("click", function(){
+  var scheduleTime = $(this).parent().attr("id");
+  var scheduleEntry = $(this).siblings(".description").val();
 
+  localStorage.setItem(scheduleTime, scheduleEntry);
+});
